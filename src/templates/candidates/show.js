1885924008cc28af85288cfecf94487b262de61f
JFree <jeffrey410469@gmail.com>
2019-07-03 20:10:58 +0800

    - Improve style

--- src/templates/candidates/show.js
+++ src/templates/candidates/show.js
@@ -5,47 +5,56 @@ import Chart from 'react-google-charts'
 
 
 const Candidate = ({ pageContext }) => {
+  let incomeRecords
   let outcomeRecords
   if (pageContext.candidate.finance_data !== null) {
+    incomeRecords = pageContext.candidate.finance_data.income.top300
     outcomeRecords = pageContext.candidate.finance_data.outcome.top300
   } else {
+    incomeRecords = []
     outcomeRecords = []
   }
-  const outComeData = []
-  if (outcomeRecords !== null) {
-    outcomeRecords.forEach((record) => {
-      outComeData.push([record.type, record.object, record.amount])
-    })
-  }
-    // Object.keys(candidateOutcome).forEach((type) => {
-  //   const records = candidateOutcome[type].records
-  //   if (records.length !== 0) {
-  //     records.forEach((record) => {
-  //       outComeData.push([type, record.object, record.amount])
-  //     })
-  //   } else {
-  //     const itemsCount = parseInt(candidateOutcome[type].item_count)
-  //     const sum = parseInt(candidateOutcome[type].sum)
-  //     const avg = sum / itemsCount
-  //     outComeData.push([type, `共 ${ itemsCount } 筆款項，平均每筆 ${avg} 元`, sum])
-  //   }
-  // })
-  outComeData.unshift(['From', 'To', '金額'])
+  const incomeData = incomeRecords.map((record) => (
+    [record.type, record.object, record.amount]
+  ))
+  const outcomeData = outcomeRecords.map((record) => (
+    [record.object, record.type, record.amount]
+  ))
+  incomeData.unshift(['From', 'To', '金額'])
+  outcomeData.unshift(['To', 'From', '金額'])
   return(
     <Layout>
       <Link to={pageContext.prevPath}>{'< 返回'}</Link>
       <h1>{ pageContext.candidate.name }</h1>
       <h3>{ pageContext.candidate.party }</h3>
       <p>得票數:{ pageContext.candidate.num_of_vote } / 得票率:{pageContext.candidate.rate_of_vote } </p>
-      <Chart
-        forceIFrame={true}
-        width={300}
-        height={'2000px'}
-        chartType="Sankey"
-        loader={<div>Loading Chart</div>}
-        data={outComeData}
-        rootProps={{ 'data-testid': '1' }}
-      />
+      <div style={{
+            display: `flex`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}>
+        <Chart
+          forceIFrame={false}
+          width={480}
+          height={'2000px'}
+          chartType="Sankey"
+          loader={<div>Loading Chart</div>}
+          data={incomeData}
+          rootProps={{ 'data-testid': '1' }}
+        />
+        <Chart
+          forceIFrame={false}
+          width={480}
+          height={'2000px'}
+          chartType="Sankey"
+          loader={<div>Loading Chart</div>}
+          data={outcomeData}
+          rootProps={{ 'data-testid': '2' }}
+        />
+
+      </div>
     </Layout>
   )
 }
